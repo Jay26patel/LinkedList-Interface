eee49f5888965d010e3fafb6530bf7b0d94b894e
Jay Patel <jaypateld2p@gmail.com>
2022-09-20 15:36:31 -0700

    First commit

---
+++ IntList.java
@@ -0,0 +1,16 @@
+import java.util.Iterator;
+
+public interface IntList<T> extends Iterable<T> {
+    public void add(T value);
+    public void add(int index, T value);
+    public T get(int index);
+    public int indexOf(T value);
+    public Iterator iterator();
+    public boolean isEmpty();
+    public void remove(int index);
+    public void set(int index, T value);
+    public int size();
+    public boolean contains(T value);
+}
+
+
